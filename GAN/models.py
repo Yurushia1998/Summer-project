4b66bfa046dfe7b56b18daca382c0c441fd31682
Macbook Pro <macbookpro@Yurushia.local>
2021-01-31 21:46:34 +1030

    Fix models

--- GAN/models.py
+++ GAN/models.py
@@ -8,7 +8,7 @@ class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
         
-        ip_emb = 42560
+        ip_emb = 43890
         emb1 = 1024
         emb2 = 256
         emb3 = 64
@@ -17,22 +17,22 @@ class Discriminator(nn.Module):
         
         self.layer1 = nn.Sequential(
         nn.Linear(ip_emb, emb1),
-        nn.LeakyReLU(0.2),
-        nn.Dropout(0.3))
+        nn.BatchNorm1d(emb1),
+        nn.LeakyReLU(0.2))
 
         
         self.layer2 = nn.Sequential(
         nn.Linear(emb1, emb2),
+        nn.BatchNorm1d(emb2),
         nn.LeakyReLU(0.2),
-        nn.Dropout(0.3),
         nn.Linear(emb2, emb3),
-        nn.LeakyReLU(0.2),
-        nn.Dropout(0.3))
+        nn.BatchNorm1d(emb3),
+        nn.LeakyReLU(0.2))
         
         self.layer3 = nn.Sequential(
         nn.Linear(emb3, emb4),
-        nn.LeakyReLU(0.2),
-        nn.Dropout(0.3))
+        nn.BatchNorm1d(emb4),
+        nn.LeakyReLU(0.2))
         
         self.layer_out = nn.Sequential(
         nn.Linear(emb4, out_emb),
@@ -61,16 +61,25 @@ class Generator(nn.Module):
         
         self.layer1 = nn.Sequential(
         nn.Conv2d(in_channels = 3, out_channels =  20, kernel_size = (5,5)),
-        nn.MaxPool2d(kernel_size = (2,2),stride = 2),
-        nn.Dropout(0.3))
+        nn.BatchNorm2d(20),
+        nn.ReLU())
             
         self.layer2 = nn.Sequential(
         nn.Conv2d(in_channels = 20,out_channels = 20, kernel_size = (5,5)),
-        nn.MaxPool2d(kernel_size = (2,2),stride = 2),
-        nn.Dropout(0.3),
-        nn.Conv2d(in_channels = 20,out_channels = 10, kernel_size = (5,5)),
-        nn.MaxPool2d(kernel_size = (2,2),stride = 2),
-        nn.Dropout(0.3),
+        nn.BatchNorm2d(20),
+        nn.ReLU(),
+        nn.Conv2d(in_channels = 20,out_channels = 20,kernel_size = (5,5),stride = 2),
+        nn.BatchNorm2d(20),
+        nn.ReLU(),
+        nn.Conv2d(in_channels = 20,out_channels = 10, kernel_size = (3,3)),
+        nn.BatchNorm2d(10),
+        nn.ReLU(),
+        nn.Conv2d(in_channels = 10,out_channels = 10,kernel_size = (3,3),stride = 2),
+        nn.BatchNorm2d(10),
+        nn.ReLU(),
+        nn.Conv2d(in_channels = 10,out_channels = 10,kernel_size = (3,3),stride = 2),
+        nn.BatchNorm2d(10),
+        nn.ReLU(),
         nn.Flatten(),
          nn.Tanh())
         
@@ -78,11 +87,11 @@ class Generator(nn.Module):
         
     def forward(self, x):
         
-        #print("Size before: ",x.size())
+        #print("Size Generator before: ",x.size())
         x = self.layer1(x)
-        #print("Size before 1: ",x.size())
+        #print("Size Generator before 1: ",x.size())
         x = self.layer2(x)
-        #print("Size before 2: ",x.size())
+        #print("Size Generator before 2: ",x.size())
       
         return x
 
@@ -91,7 +100,7 @@ class KeyPointDetector(nn.Module):
     def __init__(self):
         super().__init__()
         
-        ip_emb = 42560
+        ip_emb = 43890
         emb1 = 256
         emb2 = 512
         emb3 = 1024
--- GAN/train.py
+++ GAN/train.py
@@ -10,12 +10,14 @@ from Dataloader import Dataset6D
 import warnings
 import math
 import numpy as np
+import os
+import json
 warnings.filterwarnings("ignore")
 
 torch.manual_seed(0)
 np.random.seed(0)
 batches = 8
-num_epoches = 40
+num_epoches = 10
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 is_cuda = torch.cuda.is_available()
 model_save_path = "Models/Final_model_"+str(num_epoches)+".pth"
@@ -23,19 +25,27 @@ trans = transforms.Compose([transforms.ToTensor()])
 
 
 
-#traindata = [(data,label) for (data,label) in traindata if label==0]
-#traindata = traindata[:5000]
-#batches = 500
-train_data_real_path =  "test"
+train_data_real_path =  "/Users/macbookpro/Downloads/Adelaide_Academic_Year_2020/Summer project/Bo Chen/Summer-project/GAN_data/real"
+test_data_real_path =  "/Users/macbookpro/Downloads/Adelaide_Academic_Year_2020/Summer project/Bo Chen/Summer-project/GAN_data/test_real"
+test_data_fake_path =  "/Users/macbookpro/Downloads/Adelaide_Academic_Year_2020/Summer project/Bo Chen/Summer-project/GAN_data/test_fake"
+
 key_points_path = "lm_fps"
-train_data_fake_path = "train"
+train_data_fake_path = "/Users/macbookpro/Downloads/Adelaide_Academic_Year_2020/Summer project/Bo Chen/Summer-project/GAN_data/fake"
+output_save_path = "Output_test"
+load_path = "Models/Final_model_"+str(num_epoches)+".pth"
 
 train_real =  Dataset6D(train_data_real_path,key_points_path,num_objects = 15,   trans = trans,type_data = "real")
 train_fake= Dataset6D(train_data_fake_path,key_points_path, num_objects = 15,  trans = trans,type_data = "fake")
+test_real = Dataset6D(test_data_real_path,key_points_path,num_objects = 15,   trans = trans,type_data = "real")
+test_fake = Dataset6D(test_data_fake_path,key_points_path,num_objects = 15,   trans = trans,type_data = "real")
 
 
 train_real_loader = torch.utils.data.DataLoader(train_real, batch_size=batches, shuffle = False, num_workers = 0)
 train_fake_loader =torch.utils.data.DataLoader(train_fake, batch_size=batches, shuffle = False, num_workers = 0)
+test_real_loader = torch.utils.data.DataLoader(test_real, batch_size=1, shuffle = False, num_workers = 0)
+test_fake_loader = torch.utils.data.DataLoader(test_fake, batch_size=1, shuffle = False, num_workers = 0)
+
+
 
 discriminator = Discriminator()
 generator = Generator()
@@ -85,118 +95,175 @@ keyerrors = []
 dxcumul = []
 gxcumul = []
 
+def train():
+    for epoch in range(num_epoches):
+        dx = 0
+        gx = 0
+        derr = 0
+        gerr = 0
+        keyerr = 0
+        num_iterations = math.floor(train_real.__len__()/batches)
+        #print("Number_iteration: ",num_iterations," ",train_real.__len__()," ",train_fake.__len__())
+       
+        dataloader_real_iterator = iter(train_real_loader)
+        dataloader_fake_iterator = iter(train_fake_loader)
 
-for epoch in range(num_epoches):
-    dx = 0
-    gx = 0
-    derr = 0
-    gerr = 0
-    keyerr = 0
-    num_iterations = math.floor(train_real.__len__()/batches)
-    #print("Number_iteration: ",num_iterations," ",train_real.__len__()," ",train_fake.__len__())
-   
-    dataloader_real_iterator = iter(train_real_loader)
-    dataloader_fake_iterator = iter(train_fake_loader)
+        for i in range(num_iterations):
+            discriminator.train()
+            generator.train()
+            keypoint_detector.train()
+            pos_sample_real = next(dataloader_real_iterator)["img"]
+            pos_sample_fake = next(dataloader_fake_iterator)
+            label_keypoints = pos_sample_fake["key_points"]
+            #print("Before: ",label_keypoints.shape)
+            
+            label_keypoints_flatten = label_keypoints.view(-1,256).float()
+            #print("After: ",label_keypoints_flatten.shape)
+            pos_sample_fake = pos_sample_fake["img"]
+            # Training Discriminator network
+            discrim_optim.zero_grad()
+            generator.require_gradient = False
+            pos_sample_real = pos_sample_real.to(device) 
+            pos_sample_fake = pos_sample_fake.to(device) 
+            label_keypoints_flatten = label_keypoints_flatten.to(device) 
+            '''
+            print("SHape real: ",pos_sample_real.size())
+            print("SHape keypoints: ",label_keypoints.size())
+            print("SHape fake: ",pos_sample_fake.size())
+            '''
+            pos_sample = generator(pos_sample_real)
+            #print("Shape generator: ",pos_sample.size())
+            pos_predicted = discriminator(pos_sample)
+            pos_error = criterion(pos_predicted, get_nearones(batches))
 
+            '''
+            print()
+            print("Calling generator")
+            '''
+            neg_samples = generator(pos_sample_fake)
+            #print("GENERATOR: ",neg_samples.size())
+            neg_predicted = discriminator(neg_samples)
+            neg_error = criterion(neg_predicted, get_nearzeros(batches))
+            
+            discriminator_error = pos_error + neg_error
+            discriminator_error.backward()
+            discrim_optim.step()
+            
+            generator.require_gradient = True
+            discriminator.require_gradient = False
+
+            # Training generator network
+            generat_optim.zero_grad()
+            gen_samples = generator(pos_sample_fake)
+            gen_predicted = discriminator(gen_samples)
+            generator_error = criterion(gen_predicted, get_nearones(batches))
+            generator_error.backward()
+            generat_optim.step()
+            gen_samples = gen_samples.detach()
+
+            keypoint_detector_optim.zero_grad()
+            
+            keypoint_predicted = keypoint_detector(gen_samples)
+            #print("SHape: ",keypoint_predicted.size()," ",label_keypoints_flatten.size())
+            keypoint_detector_error = criterion_keypoints(keypoint_predicted, label_keypoints_flatten)
+            keypoint_detector_error.backward()
+            keypoint_detector_optim.step()
+
+            discriminator.require_gradient = True
+            
+            derr += discriminator_error
+            gerr += generator_error
+            keyerr += keypoint_detector_error
+            dx += pos_predicted.data.mean()
+            gx += neg_predicted.data.mean()
+            #print("End an iteration")
+            
+        print(f'Epoch:{epoch}.. D x : {dx/10:.4f}.. G x: {gx/10:.4f}.. D err : {derr/10:.4f}.. G err: {gerr/10:.4f}.. K err: {keyerr/10:.4f}')
+        
+        
+        derrors.append(dx/10)
+        gerrors.append(gx/10)
+        keyerrors.append(keyerr)
+        save_state = {"num_epochs":batches,"generator":generator.state_dict(),"discriminator":discriminator.state_dict(),"keypoint_detector":keypoint_detector.state_dict(),"optimizer_generator":generat_optim.state_dict(),
+                    "optimizer_discriminator":discrim_optim.state_dict(),"optimizer_keypoint":keypoint_detector_optim.state_dict(),"derrors":derrors,"gerrors":gerrors,"keyerrors":keyerrors}
+        torch.save(save_state, model_save_path)
+
+
+def test(discriminator,generator,keypoint_detector,load_path,test_real_loader,output_save_path,batches = 1):
+    num_iterations = math.ceil(test_real_loader.__len__()/batches)
+    file = torch.load(load_path,map_location=torch.device('cpu'))
+    discriminator.load_state_dict(file["discriminator"])
+    generator.load_state_dict(file["generator"])
+    keypoint_detector.load_state_dict(file["keypoint_detector"])
+    discriminator.eval()
+    generator.eval()
+    keypoint_detector.eval()
+    dataloader_real_iterator = iter(test_real_loader)
+    all_result = {}
+    curr_obj = 1
     for i in range(num_iterations):
-        discriminator.train()
-        generator.train()
-        keypoint_detector.train()
-        pos_sample_real = next(dataloader_real_iterator)["img"]
-        pos_sample_fake = next(dataloader_fake_iterator)
-        label_keypoints = pos_sample_fake["key_points"]
-        label_keypoints_flatten = label_keypoints.view(-1,256).float()
+        
+        pos_sample_real = next(dataloader_real_iterator)
+        
+        name = pos_sample_real["name"][0].split("/")[-1].split(".")[0]
+        pos_sample_real = pos_sample_real["img"]
+        pos_sample_real = pos_sample_real.to(device) 
+        pos_sample = generator(pos_sample_real)
+        keypoint_predicted = keypoint_detector(pos_sample)
+        all_result[name]=keypoint_predicted.cpu().detach().numpy().tolist()
+        if i%5 == 4:
+            with open(os.path.join(output_save_path,'output_'+str(curr_obj)+'.json'), 'w') as outfile:
+                json.dump(all_result, outfile)
+            curr_obj += 1
+            all_result = {}
+
+def test_embedding(discriminator,generator,keypoint_detector,load_path,test_fake_real_loader,test_real_real_loader,output_save_path,batches = 1):
+    num_iterations = math.ceil(test_real_loader.__len__()/batches)
+    file = torch.load(load_path,map_location=torch.device('cpu'))
+    discriminator.load_state_dict(file["discriminator"])
+    generator.load_state_dict(file["generator"])
+    keypoint_detector.load_state_dict(file["keypoint_detector"])
+    discriminator.eval()
+    generator.eval()
+    keypoint_detector.eval()
+    dataloader_real_iterator = iter(test_real_loader)
+    all_result = {}
+    curr_obj = -1
+    all_real = [[] for i in range(5)]
+    all_fake = [[] for i in range(5)]
+    #internal_dis_fake = 
+    #internal_dis_real = 
+    for i in range(num_iterations):
+        
+        pos_sample_real = next(dataloader_real_iterator)
+        
+        name = pos_sample_real["name"][0].split("/")[-1].split(".")[0]
+        pos_sample_real = pos_sample_real["img"]
+        pos_sample_real = pos_sample_real.to(device) 
+        pos_sample_real_emb = generator(pos_sample_real)
+        pos_sample_real_dis_output = discriminator(pos_sample_real_emb).cpu().detach().numpy()
+        pos_sample_real_emb = pos_sample_real_emb.cpu().detach().numpy()
+
+
+        pos_sample_fake = next(dataloader_real_iterator)
+        
+        name = pos_sample_fake["name"][0].split("/")[-1].split(".")[0]
         pos_sample_fake = pos_sample_fake["img"]
-        # Training Discriminator network
-        discrim_optim.zero_grad()
-        generator.require_gradient = False
-        pos_sample_real = pos_sample_real.to(device) 
         pos_sample_fake = pos_sample_fake.to(device) 
-        label_keypoints_flatten = label_keypoints_flatten.to(device) 
-        '''
-        print("SHape real: ",pos_sample_real.size())
-        print("SHape keypoints: ",label_keypoints.size())
-        print("SHape fake: ",pos_sample_fake.size())
-        '''
-        pos_sample = generator(pos_sample_real)
-        pos_predicted = discriminator(pos_sample)
-        pos_error = criterion(pos_predicted, get_nearones(batches))
+        pos_sample_fake_emb = generator(pos_sample_fake)
+        pos_sample_fake_dis_output = discriminator(pos_sample_fake_emb).cpu().detach().numpy()
+        pos_sample_fake_emb = pos_sample_fake_emb.cpu().detach().numpy()
+        print("Value fake: ",pos_sample_fake_emb)
+        print("After dis: ",pos_sample_real_dis_output," ",pos_sample_fake_dis_output)
+        print("Index: ",i,"Distance is: ",np.linalg.norm(pos_sample_real_emb-pos_sample_fake_emb)," ",np.linalg.norm(pos_sample_real_dis_output-pos_sample_fake_dis_output))
+        if i%5 == 0:
+            curr_obj += 1
+        print("Curr obj: ",curr_obj)
+        all_fake[curr_obj].append(pos_sample_fake_emb)
+        all_real[curr_obj].append(pos_sample_real_emb)
+            
+            
+#test(discriminator,generator,keypoint_detector,load_path,test_real_loader,output_save_path)
+train()
+#test_embedding(discriminator,generator,keypoint_detector,load_path,test_fake_loader,test_real_loader,output_save_path)
 
-        '''
-        print()
-        print("Calling generator")
-        '''
-        neg_samples = generator(pos_sample_fake)
-        #print("GENERATOR: ",neg_samples.size())
-        neg_predicted = discriminator(neg_samples)
-        neg_error = criterion(neg_predicted, get_nearzeros(batches))
-        
-        discriminator_error = pos_error + neg_error
-        discriminator_error.backward()
-        discrim_optim.step()
-        
-        generator.require_gradient = True
-        discriminator.require_gradient = False
-
-        # Training generator network
-        generat_optim.zero_grad()
-        gen_samples = generator(pos_sample_fake)
-        gen_predicted = discriminator(gen_samples)
-        generator_error = criterion(gen_predicted, get_nearones(batches))
-        generator_error.backward()
-        generat_optim.step()
-        gen_samples = gen_samples.detach()
-
-        keypoint_detector_optim.zero_grad()
-        
-        keypoint_predicted = keypoint_detector(gen_samples)
-        #print("SHape: ",keypoint_predicted.size()," ",label_keypoints_flatten.size())
-        keypoint_detector_error = criterion_keypoints(keypoint_predicted, label_keypoints_flatten)
-        keypoint_detector_error.backward()
-        keypoint_detector_optim.step()
-
-        discriminator.require_gradient = True
-        
-        derr += discriminator_error
-        gerr += generator_error
-        keyerr += keypoint_detector_error
-        dx += pos_predicted.data.mean()
-        gx += neg_predicted.data.mean()
-
-        
-    print(f'Epoch:{epoch}.. D x : {dx/10:.4f}.. G x: {gx/10:.4f}.. D err : {derr/10:.4f}.. G err: {gerr/10:.4f}')
-    
-    
-    derrors.append(dx/10)
-    gerrors.append(gx/10)
-    keyerrors.append(keyerr)
-    save_state = {"num_epochs":batches,"generator":generator.state_dict(),"discriminator":discriminator.state_dict(),"keypoint_detector":keypoint_detector.state_dict(),"optimizer_generator":generat_optim.state_dict(),
-                "optimizer_discriminator":discrim_optim.state_dict(),"optimizer_keypoint":keypoint_detector_optim.state_dict(),"derrors":derrors,"gerrors":gerrors,"keyerrors":keyerrors}
-    torch.save(save_state, model_save_path)
-    '''
-    if epoch %10 ==0:
-        plotimage(is_cuda)
-    '''
-'''
-# Plotting the errors
-plt.plot(range(2000),[x.item() for x in derrors], color='r')
-plt.plot(range(2000),[y.item() for y in gerrors], color='g')
-plt.show()
-
-# Images created by Generator network
-for i in range(10):
-    plotimage(is_cuda)
-'''
-
-def test(model,train_real_loader):
-    num_iterations = math.ceil(train_real_loader.__len__()/batches)
-
-    dataloader_real_iterator = iter(train_real_loader)
-
-    for i in range(num_iterations):
-        discriminator.eval()
-        generator.eval()
-        keypoint_detector.eval()
-        pos_sample_real = next(dataloader_real_iterator)["img"]
-        pos_sample_real = pos_sample_real.to(device) 
-        pos_sample = generator(pos_sample_real)
